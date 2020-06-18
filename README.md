67f3c2b745a32b2e8c924d691f944fe905da779c
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2020-06-19 00:25:00 +0200

    materials v 1.0

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 
 Workshop *Explanation and exploration of machine learning models with R and DALEX* at [eRum 2020](https://2020.erum.io/) 
 
+Tutors from [MI2DataLab](https://mi2-warsaw.github.io/)
+
+* Przemysław Biecek
+* Anna Kozak
+* Szymon Maksymiuk
+
+## Prepare yourself
+
+Make sure that you have installed RStudio and latest CRAN versions of the DALEX package (v 1.2 or newer), ranger and rms.
+
+## Materials
+
+* [Cheatsheet with all important materials](Cheatsheet.pdf)
+* [Slides for the workshop](Slides.pdf)
+* [R snippets for the workshop](Snippets.R)
+
---
+++ Snippets.R
@@ -0,0 +1,178 @@
+#
+# 0. Make sure that you have all package installed
+
+library("DALEX")
+library("ranger")
+library("ggplot2")
+library("rms")
+
+#
+# 1. Look at the data we'll be working on
+
+head(titanic_imputed)
+
+#
+# 2. Let's train two classification models
+# you can try also other models if you like
+
+model_ranger <- ranger(survived ~ ., data = titanic_imputed, 
+                       classification = TRUE, probability = TRUE)
+
+model_rms <- lrm(survived ~ rcs(age)*gender + rcs(fare) + 
+                   class, data = titanic_imputed)
+
+#
+# 3. The models are different, to work with them in a uniform way we need an adapter
+# created with explain() function
+
+exp_ranger <- explain(model_ranger,
+                      data = titanic_imputed[,1:7],
+                      y = titanic_imputed$survived)
+
+predict(exp_ranger, titanic_imputed[1,])
+
+
+
+#
+# 4. Other arguments are useful too
+
+exp_rms <- explain(model_rms,
+                   data = titanic_imputed[,1:7], 
+                   y = titanic_imputed$survived,
+                   predict_function = function(m, x) 
+                     predict(m, x, type = "fitted"),
+                   label = "Logistic with splines")
+
+#
+# 5. The adapter stores some useful information about the model
+
+exp_rms$model_info
+
+#
+# 6. First step in the exploration are measures of performance
+# performance is calculated on data in explainer
+# use update_data to set a new dataset
+
+mp_ranger <- model_performance(exp_ranger)
+mp_ranger
+
+mp_rms <- model_performance(exp_rms)
+mp_rms
+
+#
+# 7. it is easier to compare models with charts
+
+plot(mp_ranger, mp_rms, geom = "roc")
+
+plot(mp_ranger, mp_rms, geom = "lift")
+
+plot(mp_ranger, mp_rms, geom = "boxplot")
+
+#
+# 8. Let's focus on a single observations
+
+henry <- titanic_imputed[1,]
+
+predict(exp_ranger, henry)
+
+#
+# 9. Variable attributions - Shapley valyes
+
+sh_ranger <- predict_parts(exp_ranger, henry, type = "shap")
+
+plot(sh_ranger, show_boxplots = FALSE) + 
+   ggtitle("Shapley values for Henry","")
+
+#
+# 10. Variable attributions - Break-down values
+
+bd_ranger <- predict_parts(exp_ranger, henry, type = "break_down_interactions")
+bd_ranger
+plot(bd_ranger, show_boxplots = FALSE) + 
+  ggtitle("Break down values for Henry","") + 
+  scale_y_continuous("",limits = c(0.09,0.33))
+
+#
+# 11. Variable attributions - Break-down with specified order
+
+bd_ranger <- predict_parts(exp_ranger, henry, 
+                    order = c("age", "gender", "fare", "class",
+                            "parch", "sibsp", "embarked"))
+ 
+plot(bd_ranger)  + 
+  scale_y_continuous("",limits = c(0.09,0.33))
+
+#
+# 11. Variable importance 
+
+mp_ranger <- model_parts(exp_ranger, type = "difference")
+
+plot(mp_ranger, show_boxplots = FALSE) + 
+  ggtitle("Variable importance","")
+
+#
+# 12. Variable importance for two models
+
+mp_ranger <- model_parts(exp_ranger)
+
+mp_rms <- model_parts(exp_rms)
+
+plot(mp_ranger, mp_rms, show_boxplots = FALSE, bar_width = 5) + 
+  ggtitle("Variable importance","")
+
+#
+# 13. Profiles for a single observation
+
+cp_ranger <- predict_profile(exp_ranger, henry)
+
+plot(cp_ranger, variables = c("age", "fare"))
+
+#
+# 14. Profiles for two models
+
+cp_rms <- predict_profile(exp_rms, henry)
+
+plot(cp_ranger, cp_rms, variables = "age", color = "_label_") + 
+  ggtitle("Ceteris Paribus for Henry")
+
+#
+# 15. How important are variables
+
+cp_rms <- predict_parts(exp_rms, henry, type = "oscillations")
+
+#
+# 16. For categorical variables
+
+plot(cp_ranger, variables = "class", categorical_type = "bars") + 
+  ggtitle("Ceteris Paribus for Henry")
+
+#
+# 17. Model profile - aveage across individual CPs
+
+mp_ranger <- model_profile(exp_ranger)
+
+plot(mp_ranger, variables = "age", geom = "points") + 
+   ggtitle("Partial dependence","")
+
+#
+# 18. Model profile - aveage calculated in groups
+
+mp_ranger <- model_profile(exp_ranger, groups = "gender")
+
+plot(mp_ranger, variables = "age") + 
+   ggtitle("Partial dependence","")
+
+#
+# 19. Model profile - aveage calculated in clusters
+
+mp_ranger <- model_profile(exp_ranger, k = 3, center = TRUE)
+
+plot(mp_ranger, variables = "age") + 
+   ggtitle("Partial dependence 3 clusters","")
+
+#
+# 20. modelStudio dashboard
+
+library(modelStudio)
+
+modelStudio(exp_ranger)
